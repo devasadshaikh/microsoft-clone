d8613d18551387c9d1c972d9a6dd2fb644b1ea0f
devasadshaikh <officialshaikhasad@gmail.com>
2023-12-10 20:49:14 +0500

    asad
    okk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import logo from '../../public/logo.png'
 import search from '../../public/search.png'
@@ -7,16 +8,21 @@ import laptop from '../../public/laptop.png'
 import w1 from '../../public/w1.png'
 import w2 from '../../public/w2.png'
 import w3 from '../../public/w3.png'
+import close from '../../public/close.png'
+import menu from '../../public/menu.png'
+import { useState } from 'react'
 export default function Home() {
+
+  const [collapse, setCollapse] = useState<boolean>(false)
+
   return (
     <div className=' w-full mx-auto'>
       <nav className='h-14 bg-white flex justify-between'>
         <div className=' flex justify-center items-center md:-order-3'>
 
-          <div className=' ml-4 md:hidden'>
-            <div className='w-6 h-0.5 bg-black my-1'></div>
-            <div className='w-6 h-0.5 bg-black my-1'></div>
-            <div className='w-6 h-0.5 bg-black my-1'></div>
+          <div className=' ml-4 md:hidden flex items-center'>
+            <button onClick={() => setCollapse(true)}><Image src={menu} alt='menu' width={30} height={30} className=' cursor-pointer' /></button>
+
           </div>
 
           <div className=' md:hidden'><Image src={search} alt='search' width={20} height={20} className=' rotate-90 ml-4' /></div>
@@ -25,12 +31,15 @@ export default function Home() {
         <div className=' flex items-center  md:-order-4 md:ml-4'>
           <div><Image src={logo} alt='logo' width={120} height={120} className=' ml-8' /></div>
 
-          <div className=' text-sm absolute w-fit md:static md:w-auto bg-transparent md:bg-transparent inset-0 md:flex md:mx-4 md:space-x-3 -translate-x-96 md:translate-x-0'>
-            <div className=' hover:underline hover:underline-offset-8 cursor-pointer'>Microsoft 365</div>
-            <div className=' hover:underline hover:underline-offset-8 cursor-pointer'>Office</div>
-            <div className=' hover:underline hover:underline-offset-8 cursor-pointer'>Copilot</div>
-            <div className=' hover:underline hover:underline-offset-8 cursor-pointer'>Windows</div>
-            <div className=' hover:underline hover:underline-offset-8 cursor-pointer'>Support</div>
+          <div className={` bg-[#f2f2f2] h-1/2 w-full  mt-14 text-sm absolute ${collapse ? "" : "ms-[-100%]"} md:mb-20 md:static md:w-auto md:bg-transparent inset-0 md:flex md:mx-4 md:space-x-3  md:translate-x-0 transition-all ease-in-out duration-500`}>
+            <button className=' cursor-pointer my-4 ml-5 md:hidden' onClick={() => setCollapse(false)}>X</button>
+            <div className=' md:hover:underline md:hover:underline-offset-8 cursor-pointer my-4 ml-5'>Microsoft 365</div>
+            <div className=' md:hover:underline md:hover:underline-offset-8 cursor-pointer my-4 ml-5'>Office</div>
+            <div className=' md:hover:underline md:hover:underline-offset-8 cursor-pointer my-4 ml-5' >Copilot</div>
+            <div className=' md:hover:underline md:hover:underline-offset-8 cursor-pointer my-4 ml-5'>Windows</div>
+            <div className=' md:hover:underline md:hover:underline-offset-8 cursor-pointer my-4 ml-5'>Support</div>
+
+
           </div>
 
         </div>
